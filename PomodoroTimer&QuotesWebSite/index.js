dfd698d2825148cc2aca5556f2c36511a2daa7fd
Kevser Busra YILDIRIM <kevserbusra.yildirim@stu.fsm.edu.tr>
2021-12-23 11:29:03 +0300

    Update index.js

--- PomodoroTimer&QuotesWebSite/index.js
+++ PomodoroTimer&QuotesWebSite/index.js
@@ -1,4 +1,25 @@
 
+const startingMinutes = 25;
+let time = startingMinutes * 60;
+
+const countDown = document.getElementById('countdown')
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown(){
+    const minutes = Math.floor(time/60);
+    let seconds = time % 60;
+    countdownEl.innerHtml = `${minutes}: ${seconds}`;
+    time--;
+}
+
+function stopCountdown(){
+
+}
+
+function resetCountdown(){
+
+}
 let list = []
 const text = document.getElementById("text")
 const author = document.getElementById("author")
