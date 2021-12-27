81307091c1e2e611aa59a8ecc69ac2bc5895a747
Kevser Busra YILDIRIM <kevserbusra.yildirim@stu.fsm.edu.tr>
2021-12-27 17:16:35 +0300

    Update index.js
    Added stop, reset buttons and minutes countdown codes.

--- PomodoroTimer&QuotesWebSite/index.js
+++ PomodoroTimer&QuotesWebSite/index.js
@@ -1,25 +1,57 @@
+var alertSound = new Audio('./audio/ship_bell.mp3');
+alertSound.loop = false;
+var pomodoroCountdownId;
+var pomodoroButtonPressed = false;
+var pomodoroResetButton = document.getElementById('resetButton');
+var resetCheck = pomodoroResetButton.value;
+var minutesSelect = document.getElementById('minutes-select');
+var pomodoroTime = 25 * 60;
+var pomodoroTimeLeft = document.getElementById('timeleft');
+var pomodoroStartButton = document.getElementById('startButton');
+var minute = 0;
+var seconds = 0;
+pomodoroResetButton.addEventListener('click', function() {
+  resetCheck = true;
+})
+minutesSelect.addEventListener('change', function(event) {
+  pomodoroTime = event.target.value * 60;
+})
+pomodoroStartButton.addEventListener('click', function() {
+  pomodoroCountdown();
+})
 
-const startingMinutes = 25;
-let time = startingMinutes * 60;
+// TODO: 1. Style and complete. Maybe overlay timer over a grey bell
 
-const countDown = document.getElementById('countdown')
-
-setInterval(updateCountdown, 1000);
-
-function updateCountdown(){
-    const minutes = Math.floor(time/60);
-    let seconds = time % 60;
-    countdownEl.innerHtml = `${minutes}: ${seconds}`;
-    time--;
+pomodoroShowNextSecond = function() {
+  if (resetCheck === true || pomodoroButtonPressed === false) {
+    pomodoroTimeLeft.innerHTML = '0:00';
+    pomodoroButtonPressed = false;
+    resetCheck = false;
+    clearInterval(pomodoroCountdownId);
+    pomodoroTime = minutesSelect.value * 60;
+    return;
+  };
+  if (pomodoroTime > 0) {
+    minutes = Math.floor(pomodoroTime / 60);
+    secondsInitial = pomodoroTime % 60;
+    seconds = secondsInitial <= 9 ? '0' + secondsInitial : secondsInitial;
+    pomodoroTime --;
+    pomodoroTimeLeft.innerHTML = minutes + ':' + seconds;
+  } else if (pomodoroButtonPressed && pomodoroTime === 0) {
+    pomodoroButtonPressed = false;
+    alertSound.play();
+  }
+}
+pomodoroCountdown = function() {
+  if (pomodoroButtonPressed === false) {
+    pomodoroButtonPressed = true;
+    pomodoroCountdownId = setInterval(pomodoroShowNextSecond, 1000);
+  }
 }
 
-function stopCountdown(){
 
-}
 
-function resetCountdown(){
 
-}
 let list = []
 const text = document.getElementById("text")
 const author = document.getElementById("author")
